0fed305cf0e521b6e144f1502561f780f328c216
TommyDong1998 <tommydong1998@gmail.com>
2023-01-15 20:52:58 -0800

    Create question toggle

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
 //List databases
 const databases={
     "Relational":[
-        {name:"MYSQL"},
-        {name:"Postgres"},
-        {name:"Oracle"},
-        {name:"Aurora"}
+        {name:"MYSQL",tags:["relation","consistent_immediate"]},
+        {name:"Postgres",tags:["relation","consistent_immediate"]},
+        {name:"Oracle",tags:["relation","consistent_immediate"]},
+        {name:"Aurora",tags:["relation","consistent_immediate"]}
     ],
     "Graph":[
-        {name:"Neptune"}
+        {name:"Neptune",tags:["graph"]}
     ],
     "Key Value":[
-        {name:"DynamoDB"}
+        {name:"DynamoDB",tags:["keyvalue","consistent_immediate"]}
     ],
     "Memory":[
-        {name:"Memcached"},
-        {name:"Redis"}
+        {name:"Memcached",tags:["memory"]},
+        {name:"Redis",tags:["memory"]}
     ],
     "Document":[
-        {name:"MongoDB"},
-        {name:"DocumentDB"}
+        {name:"MongoDB",tags:["document"]},
+        {name:"DocumentDB",tags:["document"]}
     ],
     "Time Series":[
-        {name:"Timeseries"}
+        {name:"Timeseries",tags:["timeseries"]}
     ],
     "Ledger":[
-        {name:"QLDB"}
+        {name:"QLDB",tags:["ledger"]}
     ]
+};
+
+const questions={
+    "Database Type":[
+        {name:"Any",tags:[]},
+        {name:"Relation",tags:["relation"]},
+        {name:"Graph",tags:["graph"]},
+        {name:"Memory",tags:["memory"]},
+        {name:"Document",tags:["document"]},
+        {name:"Key Value",tags:["keyvalue"]},
+        {name:"Time Series",tags:["timeseries"]},
+        {name:"Ledger",tags:["ledger"]}
+    ],
+    "Consistent":[
+        {name:"Any",tags:[]},
+        {name:"Immediate",tags:["consistent_immediate"]}
+    ]
+
 };
 
 let dbElement=document.querySelector('#databases');
@@ -37,23 +55,71 @@ function createDiv(c){
     return d;
 }
 
+let filtersTags=[];
+
 //List all databases and add element property to the database
 //Ex: database["Relational"][0].element would reference the html element
-Object.keys(databases).forEach(dbtype => {
-    let dbtypeElement=createDiv('dbtype')
-    dbElement.appendChild(dbtypeElement);
+function generateDatabaseOutput(){
+    dbElement.innerHTML="";
+    Object.keys(databases).forEach(dbtype => {
+        let dbtypeElement=createDiv('dbtype')
+        dbElement.appendChild(dbtypeElement);
 
-    let category=createDiv('category')
-    category.innerText=dbtype;
-    dbtypeElement.appendChild(category);
+        let category=createDiv('category')
+        category.innerText=dbtype;
+        dbtypeElement.appendChild(category);
+
+        let list=createDiv('list');
+        dbtypeElement.appendChild(list);
+
+        databases[dbtype].filter((db)=>{
+            return filtersTags.every(v => db.tags.includes(v.tag));
+        }).forEach((db,idx)=>{
+            let dbElement=createDiv('db');
+            databases[dbtype][idx].element=dbElement;
+            dbElement.innerText=db.name;
+            list.appendChild(dbElement);
+        })
+    });
+}
+generateDatabaseOutput();
+
+let selectionElement=document.querySelector('#selection');
+let questionID=0;
+
+
+function ToggleByQuestion(category,question){
+    //Remove all the tags with same category
+    filtersTags=filtersTags.filter((filter)=>{
+        filter.category!=category;
+    });
+    //Add new tag
+    question.tags.forEach(tag => filtersTags.push({"category":category,tag:tag}))
+    generateDatabaseOutput();
+}
+
+//Generate questions
+Object.keys(questions).forEach(category => {
+    let questionCategoryElement=createDiv('questionCategory')
+    selectionElement.appendChild(questionCategoryElement);
+
+    let categoryElement=createDiv('category')
+    categoryElement.innerText=category;
+    questionCategoryElement.appendChild(categoryElement);
 
     let list=createDiv('list');
-    dbtypeElement.appendChild(list);
+    questionCategoryElement.appendChild(list);
 
-    databases[dbtype].forEach((db,idx)=>{
-        let dbElement=createDiv('db');
-        databases[dbtype][idx].element=dbElement;
-        dbElement.innerText=db.name;
-        list.appendChild(dbElement);
+    questions[category].forEach((question)=>{
+        let questionElement=createDiv('question');
+        questionElement.innerHTML="<input id='questionid"+questionID+"' type='radio' name='"+category+"'/>"
+        
+    
+       questionElement.innerHTML+="<label for='questionid"+questionID+"'>"+question.name+"</label>";
+        questionID+=1;
+        list.appendChild(questionElement);
+        questionElement.querySelector('input').onclick=()=>{
+            ToggleByQuestion(category,question);
+        }
     })
 });
