01c5e49609e796598f5bb83fc292ed44c1f35feb
TommyDong1998 <tommydong1998@gmail.com>
2023-01-17 02:06:59 -0800

    Added tooltip

--- script.js
+++ script.js
@@ -1,57 +1,160 @@
 //List databases
 const databases = {
-  Relational: [
-    { name: "MYSQL", tags: ["relation", "consistent_immediate", "opensource","commonusecase_login"] },
-    {
-      name: "SQLite",
-      tags: ["relation", "consistent_immediate", "opensource","commonusecase_login"],
-    },
-    {
-      name: "Postgres",
-      tags: ["relation", "consistent_immediate","unstructured", "opensource","commonusecase_login"],
-    },
-    {
-      name: "Oracle",
-      tags: ["relation", "consistent_immediate", "proprietary","commonusecase_login"],
-    },
-    {
-      name: "Aurora",
-      tags: ["relation", "consistent_immediate", "proprietary","commonusecase_login"],
-    },
-    {
-      name: "IBM Db2",
-      tags: ["relation", "consistent_immediate", "proprietary","commonusecase_login"],
-    },
-    {
-      name: "Microsoft SQL",
-      tags: ["relation", "consistent_immediate", "proprietary","commonusecase_login"],
-    }
-  ],
-  Graph: [
-    { name: "Neptune", tags: ["graph", "proprietary","commonusecase_visualize"] },
-    { name: "Neo4J", tags: ["graph", "opensource","commonusecase_visualize"] }
-  ],
-  "Key Value": [
-    {
-      name: "DynamoDB",
-      tags: ["keyvalue", "consistent_immediate", "proprietary","horizontal_scaling","commonusecase_login"],
-    },
-    { name: "CosmosDB", tags: ["document", "keyvalue","unstructured", "proprietary","commonusecase_login","consistent_immediate"] }
-  ],
-  Memory: [
-    { name: "Memcached", tags: ["memory", "opensource"] },
-    { name: "Redis", tags: ["memory", "opensource"] },
-  ],
-  Document: [
-    { name: "MongoDB", tags: ["document", "opensource", "unstructured", "horizontal_scaling","commonusecase_login","consistent_immediate"] },
-    { name: "DocumentDB", tags: ["document", "proprietary","unstructured","commonusecase_login","consistent_immediate"] },
-    { name: "CosmosDB", tags: ["document", "keyvalue","unstructured", "proprietary","commonusecase_login","consistent_immediate"] },
-  ],
-  "Time Series": [
-    { name: "Timestream", tags: ["Timestream","proprietary"] },
-    { name: "InfluxDB", tags: ["Timestream","opensource"] },
-  ],
-  Ledger: [{ name: "QLDB", tags: ["ledger", "proprietary"] }],
+  Relational: {
+    tooltip:"If you need to structure data as relationships or if ACID compliant is a must",
+    list: [
+      {
+        name: "MYSQL",
+        tags: [
+          "relation",
+          "consistent_immediate",
+          "opensource",
+          "commonusecase_login",
+        ],
+      },
+      {
+        name: "SQLite",
+        tags: [
+          "relation",
+          "consistent_immediate",
+          "opensource",
+          "commonusecase_login",
+        ],
+      },
+      {
+        name: "Postgres",
+        tags: [
+          "relation",
+          "consistent_immediate",
+          "unstructured",
+          "opensource",
+          "commonusecase_login",
+        ],
+      },
+      {
+        name: "Oracle",
+        tags: [
+          "relation",
+          "consistent_immediate",
+          "proprietary",
+          "commonusecase_login",
+        ],
+      },
+      {
+        name: "Aurora",
+        tags: [
+          "relation",
+          "consistent_immediate",
+          "proprietary",
+          "commonusecase_login",
+        ],
+      },
+      {
+        name: "IBM Db2",
+        tags: [
+          "relation",
+          "consistent_immediate",
+          "proprietary",
+          "commonusecase_login",
+        ],
+      },
+      {
+        name: "Microsoft SQL",
+        tags: [
+          "relation",
+          "consistent_immediate",
+          "proprietary",
+          "commonusecase_login",
+        ],
+      },
+    ],
+  },
+  Graph: {
+    list: [
+      {
+        name: "Neptune",
+        tags: ["graph", "proprietary", "commonusecase_visualize"],
+      },
+      {
+        name: "Neo4J",
+        tags: ["graph", "opensource", "commonusecase_visualize"],
+      },
+    ],
+  },
+  "Key Value": {
+    list: [
+      {
+        name: "DynamoDB",
+        tags: [
+          "keyvalue",
+          "consistent_immediate",
+          "proprietary",
+          "horizontal_scaling",
+          "commonusecase_login",
+        ],
+      },
+      {
+        name: "CosmosDB",
+        tags: [
+          "document",
+          "keyvalue",
+          "unstructured",
+          "proprietary",
+          "commonusecase_login",
+          "consistent_immediate",
+        ],
+      },
+    ],
+  },
+  Memory: {
+    list: [
+      { name: "Memcached", tags: ["memory", "opensource"] },
+      { name: "Redis", tags: ["memory", "opensource"] },
+    ],
+  },
+  Document: {
+    list: [
+      {
+        name: "MongoDB",
+        tags: [
+          "document",
+          "opensource",
+          "unstructured",
+          "horizontal_scaling",
+          "commonusecase_login",
+          "consistent_immediate",
+        ],
+      },
+      {
+        name: "DocumentDB",
+        tags: [
+          "document",
+          "proprietary",
+          "unstructured",
+          "commonusecase_login",
+          "consistent_immediate",
+        ],
+      },
+      {
+        name: "CosmosDB",
+        tags: [
+          "document",
+          "keyvalue",
+          "unstructured",
+          "proprietary",
+          "commonusecase_login",
+          "consistent_immediate",
+        ],
+      },
+    ],
+  },
+  "Time Series": {
+    list: [
+      { name: "Timestream", tags: ["Timestream", "proprietary"] },
+      { name: "InfluxDB", tags: ["Timestream", "opensource"] },
+    ],
+  },
+  Ledger: { list: [{ name: "QLDB", tags: ["ledger", "proprietary"] }] },
 };
 
 const questions = {
@@ -71,7 +174,7 @@ const questions = {
     { name: "Visualize relationships", tags: ["commonusecase_visualize"] },
     { name: "Monetary transactions", tags: ["consistent_immediate"] },
   ],
-  "Consistent": [
+  Consistent: [
     { name: "Any", tags: [] },
     { name: "Immediate", tags: ["consistent_immediate"] },
   ],
@@ -81,7 +184,7 @@ const questions = {
   ],
   "Easy to horizontal scale": [
     { name: "Any", tags: [] },
-    { name: "Yes", tags: ["horizontal_scaling"] }
+    { name: "Yes", tags: ["horizontal_scaling"] },
   ],
   "Open Source": [
     { name: "Any", tags: [] },
@@ -111,18 +214,34 @@ function generateDatabaseOutput() {
 
     let category = createDiv("category");
     category.innerText = dbtype;
+
     dbtypeElement.appendChild(category);
+    /*
+    Create tooltip
+    Uses Tippy.js
+    */
+    if (databases[dbtype].tooltip) {
+      let q = document.createElement("div");
+      q.innerText = " ?";
+      q.id = "question_" + dbtype;
+      category.appendChild(q);
+
+      tippy("#question_" + dbtype, {
+        content: databases[dbtype].tooltip,
+      });
+    }
+    /*End of tooltip*/
 
     let list = createDiv("list");
     dbtypeElement.appendChild(list);
 
-    databases[dbtype]
+    databases[dbtype].list
       .filter((db) => {
         return filtersTags.every((v) => db.tags.includes(v.tag));
       })
       .forEach((db, idx) => {
         let dbElement = createDiv("db");
-        databases[dbtype][idx].element = dbElement;
+        databases[dbtype].list[idx].element = dbElement;
         dbElement.innerText = db.name;
         list.appendChild(dbElement);
       });
@@ -135,17 +254,14 @@ let questionID = 0;
 
 function ToggleByQuestion(category, question) {
   //Remove all the tags with same category
-  
+
   filtersTags = filtersTags.filter((filter) => {
-    console.log(filter.category,  category)
     return filter.category != category;
   });
-  console.log(filtersTags)
   //Add new tag
   question.tags.forEach((tag) =>
     filtersTags.push({ category: category, tag: tag })
   );
-  console.log(category,filtersTags)
   generateDatabaseOutput();
 }
 
